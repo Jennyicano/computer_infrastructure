267bf9fdb200a0396c74b1302b6302fe025bce5e
Jenny Ibanez <157984043+Jennyicano@users.noreply.github.com>
2024-11-25 21:45:24 +0000

    updating the weather.sh and leaning how to add code in markdown

--- weather.sh
+++ weather.sh
@@ -3,4 +3,8 @@
 # saving the weather data in a file with a timestamped name in the format YYYYmmdd_HHMMSS. 
 # in the data/weather directory. 
 
+date
+echo "Downloading weather data"
 wget -O data/weather/`date +”%Y%m%d_%H%M%S_athenry.json”` https://prodapi.metweb.ie/observations/athenry/today
+echo "Weather data downloaded"
+date
